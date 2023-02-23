9d972c12922e80d6af6b6d598779d90bac9af382
camfam17 <81819903+camfam17@users.noreply.github.com>
2023-02-23 22:20:45 +0200

    more bg detec

--- Main.py
+++ Main.py
@@ -1,12 +1,12 @@
 import cv2 as cv
+import random
+from collections import Counter
 
 class Classifier():
     
 	def __init__(self, images):
 		
-		print('hi')
-		
-		for i in range(1):
+		for i in range(len(images)):
 			
 			self.img = images[i]
 			
@@ -38,27 +38,55 @@ class Classifier():
 		
 		img = image.copy()
 		
-		height, width, channels = img.shape
+		height, width, _channels = img.shape
 		
 		percent = 0.05
 		hBorder = int(percent * height)
 		wBorder = int(percent * width)
 		
-		print(height, width, channels)
+		print(height, width, _channels)
 		
-		img[:hBorder, :, :] = 0 # TOP border
-		img[:, :wBorder, :] = 0 # LEFT border
+		# img[:hBorder, :, :] = 0 # TOP border
+		# img[:, :wBorder, :] = 0 # LEFT border
+		# img[:, width-wBorder:width, :] = 0 # RIGHT border
+		# img[height-hBorder:height, :, :] = 0 # BOTTOM border
+		# cv.imshow('border', img)
 		
-		cv.imshow('border', img)
 		
-		pass
+		top_points, bottom_points, left_points, right_points = [], [], [], []
+		top_colours, bottom_colours, left_colours, right_colours = [], [], [], []
+		for i in range(100):
+								##### (height, width)
+			top_points.append((random.randint(0, hBorder), random.randint(0, width)))
+			bottom_points.append((random.randint(height-hBorder, height), random.randint(0, width)))
+			left_points.append((random.randint(0, height), random.randint(0, wBorder)))
+			right_points.append((random.randint(0, height), random.randint(width-wBorder, width)))
+			
+			img[random.randrange(0, hBorder), random.randrange(0, width)] = [0, 0, 255]
+			img[random.randrange(height-hBorder, height), random.randrange(0, width)] = [0, 255, 0]
+			img[random.randrange(0, height), random.randrange(0, wBorder)] = [255, 0, 0]
+			img[random.randrange(0, height), random.randrange(width-wBorder, width)] = [0, 122, 122]
+			
+			top_colours.append(img[random.randrange(0, hBorder), random.randrange(0, width)])
+			bottom_colours.append(img[random.randrange(height-hBorder, height), random.randrange(0, width)])
+			left_colours.append(img[random.randrange(0, height), random.randrange(0, wBorder)])
+			right_colours.append(img[random.randrange(0, height), random.randrange(width-wBorder, width)])
+		
+		cv.imshow('randoms', img)
+		
+		
+		colours = [*tuple(map(tuple, top_colours)), *tuple(map(tuple, bottom_colours)), *tuple(map(tuple, left_colours)), *tuple(map(tuple, right_colours))]
+		counts = Counter(colours)
+		print(counts)
 
 
 if __name__ == '__main__':
-    
-	img1 = cv.imread('images/img1.jpg')
 	
-	images = [img1]
+	images = []
+	images.append(cv.imread('images/img1.jpg'))
+	# images.append(cv.imread('images/img2.jpg'))
+	
 	
 	c = Classifier(images)
 	
+	print('DONE')
